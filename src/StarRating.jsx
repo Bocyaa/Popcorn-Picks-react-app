8670a2ad98eb569dccc1be5a25cfe3fe0d185caf
Abdubositkhon Khabirov <xabirovbob@gmail.com>
2025-01-31 21:56:28 +0100

    New Features: User Rating, List of Watched/Rated Movies, Bug Fixes & more

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -11,7 +11,7 @@ const starContainerStyle = {
   display: 'flex',
 };
 
-StartRating.propTypes = {
+StarRating.propTypes = {
   maxRating: PropTypes.number,
   defaultRating: PropTypes.number,
   color: PropTypes.string,
@@ -20,7 +20,7 @@ StartRating.propTypes = {
   onSetRating: PropTypes.func,
 };
 
-export default function StartRating({
+export default function StarRating({
   maxRating = 5,
   color = '#fcc419',
   size = 48,
